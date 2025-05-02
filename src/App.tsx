529f4eddc554daa4b7bf250eef27e70b2333761c
Georgina Mudditt <g.k.mudditt@gmail.com>
2025-05-02 13:41:48 +0200

    add CallbackPage

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,42 @@ import "./App.css";
 import Header from "./Header";
 import Offerings from "./Offerings";
 import Footer from "./Footer";
-import { BrowserRouter as Router, useLocation } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import KidsPage from "./pages/KidsPage";
 import AdultsPage from "./pages/AdultsPage";
 import OnlinePage from "./pages/OnlinePage";
 import ApplicationPage from "./pages/ApplicationPage";
 import ProfilePage from "./pages/ProfilePage";
 import FAQPage from "./pages/FAQPage";
+import CallbackPage from "./pages/CallbackPage";
 
 export default function App() {
   return (
     <Router>
       <div className="App">
         <Header />
-        <MainContent />
+        <Routes>
+          <Route path="/auth/callback" element={<CallbackPage />} />
+          <Route path="*" element={<MainContentWithModals />} />
+        </Routes>
         <Footer />
-        <ModalRenderer />
       </div>
     </Router>
   );
 }
 
-function MainContent() {
-  return <Offerings />;
+function MainContentWithModals() {
+  return (
+    <>
+      <Offerings />
+      <ModalRenderer />
+    </>
+  );
 }
 
 function ModalRenderer() {
---
+++ src/pages/CallbackPage.tsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+
+const CallbackPage = () => {
+  useEffect(() => {
+    const hash = window.location.hash.substring(1);
+    const params = new URLSearchParams(hash);
+    const access_token = params.get("access_token");
+    const refresh_token = params.get("refresh_token");
+
+    if (access_token && refresh_token) {
+      // Construct deep link into app
+      const deepLink = `myapp://auth/callback#access_token=${access_token}&refresh_token=${refresh_token}`;
+      window.location.href = deepLink;
+    } else {
+      console.error("Tokens not found in URL.");
+    }
+  }, []);
+
+  return <p>Verifying your email…</p>;
+};
+
+export default CallbackPage;
