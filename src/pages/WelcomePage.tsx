7f1f2a4f5bd3955a14f71679c80b94da3aa23e17
Georgina Mudditt <g.k.mudditt@gmail.com>
2025-05-19 15:49:22 +0200

    add content and logo to welcome page

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -1,17 +1,27 @@
 import "../RedirectedFromApp.css";
+import Logo from "../assets/brizzle-insta-square.png";
 
 const WelcomePage = () => {
   return (
     <div className="WelcomePage">
+      <img
+        src={Logo}
+        alt="Brizzle Logo"
+        className="WelcomePage__logo"
+        width={200}
+        height={200}
+        style={{ borderRadius: 100, marginBottom: 20 }}
+      />
       <h1>Félicitations 🎉 Compte créé 😊</h1>
       <h2>
         Vous avez créé un compte avec succès sur l'application mobile de
         Brizzle.
       </h2>
-      <h2>
+      <h2>Vous pouvez maintenant vous connecter à l’application.</h2>
+      <p>
         Nous espérons que vous apprécierez l'application et nous vous souhaitons
         bonne chance pour gagner vos icônes !
-      </h2>
+      </p>
       <p>
         N'hésitez pas à nous contacter pour toute remarque concernant
         l'application :{" "}
